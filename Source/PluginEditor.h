5c65ab840b21c938bf854774ecd74ac1a54fe2a4
Tayo <92905932+tayo6@users.noreply.github.com>
2026-07-12 18:24:15 +0100

    Update PluginEditor.h

--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,18 +1,20 @@
 #pragma once
 #include "PluginProcessor.h"
 #include <juce_gui_basics/juce_gui_basics.h>
-#include <juce_graphics/juce_graphics.h>
 
 class GradientKnob : public juce::Component, private juce::Timer
 {
 public:
-    GradientKnob(juce::AudioProcessorValueTreeState& s);
+    explicit GradientKnob(juce::AudioProcessorValueTreeState& s);
     void paint(juce::Graphics& g) override;
+    void resized() override {}
     void mouseDown(const juce::MouseEvent& e) override;
     void mouseDrag(const juce::MouseEvent& e) override;
+    void mouseUp(const juce::MouseEvent& e) override;
+    void mouseWheelMove(const juce::MouseEvent&, const juce::MouseWheelDetails& w) override;
 private:
     void timerCallback() override;
-    void updateFromMouse(juce::Point<float> p);
+    void updateFromMouse(juce::Point<float> p, bool force = false);
     static juce::Colour lerpColor(float t);
     static juce::Point<float> angleToPos(juce::Point<float> c, float r, float aRad);
     void roundedLine(juce::Graphics& g, juce::Point<float> a, juce::Point<float> b, float w, juce::Colour col);
@@ -20,6 +22,7 @@ private:
     juce::AudioProcessorValueTreeState& apvts;
     juce::AudioParameterFloat* param = nullptr;
     float target = 50.0f, current = 50.0f;
+    bool isDragging = false;
 
     static constexpr float START_DEG = 120.0f;
     static constexpr float SWEEP_DEG = 300.0f;
@@ -30,7 +33,8 @@ private:
 class KnobMuseAudioProcessorEditor : public juce::AudioProcessorEditor
 {
 public:
-    KnobMuseAudioProcessorEditor(KnobMuseAudioProcessor&);
+    explicit KnobMuseAudioProcessorEditor(KnobMuseAudioProcessor&);
+    void resized() override;
 private:
     KnobMuseAudioProcessor& proc;
     GradientKnob knob;
